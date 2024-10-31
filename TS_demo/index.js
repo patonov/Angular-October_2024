aa0b1bf5a3f8f31d2e8c9e3e7ef5e56a70f9c225
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-10-31 22:10:21 +0200

    Add files via upload

---
+++ TS_demo/index.js
@@ -0,0 +1,5 @@
+var greetMsg = "Hello, there!";
+var a = 100;
+var isAuthenticated = false;
+var arr = [1, 2, 3, 4];
+console.log(arr.filter(function (x) { return x > 2; }));
---
+++ TS_demo/index.ts
@@ -0,0 +1,7 @@
+let greetMsg: string = "Hello, there!";
+let a: number = 100;
+let isAuthenticated: boolean = false;
+
+const arr: number[] = [1, 2, 3, 4];
+
+console.log(arr.filter((x) => x > 2));
